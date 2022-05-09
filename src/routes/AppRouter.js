c3867451c62a49b66d58a7152ed801bb48dc58b2
dootrex <avigill@hotmail.ca>
2022-05-08 18:21:15 -0700

    initial

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/landingPage";
 import LoginPage from "../pages/loginPage";
 import RegisterPage from "../pages/registerPage";
@@ -13,18 +13,26 @@ import FindDeveloper from "../pages/findDeveloperPage";
 
 export default function AppRouter() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/home" element={<HomePage />} />
+    <Switch>
+      <Route path="/" exact>
+        <LandingPage />
+      </Route>
+
+      <Route path="/login" exact>
+        <LoginPage />
+      </Route>
+
+      <Route path="/register" exact>
+        <Register />
+      </Route>
+      {/* <Route path="/home" element={<HomePage />} />
       <Route path="/realtordirectory" element={<FindRealtor />} />
       <Route path="/developerdirectory" element={<FindDeveloper />} />
-      <Route path="/listings" element={<MyListingsPage />} /> 
+      <Route path="/listings" element={<MyListingsPage />} />
       <Route path="/listings/:id" element={<ListingDetails />} />
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/tabs" element={<Tabs />} />
-      <Route path="/newlisting" element={<NewListingPage />} />
-    </Routes>
+      <Route path="/newlisting" element={<NewListingPage />} /> */}
+    </Switch>
   );
 }
